e7c4c7c0e859562249faf04d09c381b48b8d1aee
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-04-29 03:49:23 -0700

    Add amazon-2-x86_64

---
+++ configs/platforms/amazon-2-x86_64.rb
@@ -0,0 +1,17 @@
+platform 'amazon-2-x86_64' do |plat|
+  plat.inherit_from_default
+
+  packages = %w[
+    perl-FindBin
+    perl-lib
+    perl-IPC-Cmd
+    readline-devel
+    systemtap-sdt-devel
+    systemtap-sdt-trace
+    zlib-devel
+  ]
+
+  plat.provision_with "yum install -y #{packages.join(' ')}"
+  plat.install_build_dependencies_with 'yum install --assumeyes'
+  plat.vmpooler_template 'amazon-7-arm64'
+end
