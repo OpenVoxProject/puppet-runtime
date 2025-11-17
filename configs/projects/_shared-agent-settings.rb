f592c5d485fdca3a77cf5a76ec811b45d7af5f3a
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-11-17 10:24:46 -0800

    Remove 7.x and components
    If we need to build 7.x runtimes in the future, we can branch off a previous commit. This removes this project and associated components that only exist for this runtime.

--- configs/projects/_shared-agent-settings.rb
+++ configs/projects/_shared-agent-settings.rb
@@ -152,16 +152,7 @@ proj.setting(:host, host)
 # Load default compiler settings
 instance_eval File.read('configs/projects/_shared-compiler-settings.rb')
 
-# Always use OpenSSL 3, even on FIPS, when we're building for Ruby 3
-if ruby_version_x == "3"
-  proj.setting(:openssl_version, '3.0')
-elsif platform.name =~ /^redhatfips-/ # This is only Ruby 2 builds
-  proj.setting(:openssl_version, '1.1.1-fips')
-elsif platform.name =~ /^windowsfips-/
-  proj.setting(:openssl_version, '1.0.2')
-else
-  proj.setting(:openssl_version, '1.1.1')
-end
+proj.setting(:openssl_version, '3.0')
 
 if platform.is_windows?
   proj.setting(:gcc_root, "/usr/x86_64-w64-mingw32/sys-root/mingw")
