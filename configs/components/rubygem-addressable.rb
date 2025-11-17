8e2b07048917aa55053836be79c920a47543c957
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-11-17 15:44:18 -0800

    rubygem-addressable: Add rubygem-public_suffix dependency

--- configs/components/rubygem-addressable.rb
+++ configs/components/rubygem-addressable.rb
@@ -7,6 +7,7 @@ component 'rubygem-addressable' do |pkg, settings, platform|
   ### Maintained by update_gems automation ###
   pkg.version '2.8.7'
   pkg.sha256sum '462986537cf3735ab5f3c0f557f14155d778f4b43ea4f485a9deb9c8f7c58232'
+  pkg.build_requires 'rubygem-public_suffix'
   ### End automated maintenance section ###
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
