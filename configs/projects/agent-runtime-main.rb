1c1d20a3a3bdfa6ef9c81ff1c836aa8acdfad717
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-11-17 15:13:36 -0800

    Remove _shared-agent-components.rb
    We don't need this since we only have one agent project now. This makes it easier to maintain with automation.

--- configs/projects/agent-runtime-main.rb
+++ configs/projects/agent-runtime-main.rb
@@ -39,35 +39,82 @@ project 'agent-runtime-main' do |proj|
   proj.setting(:gem_install, "#{proj.host_gem} install --no-document --local")
 
   ########
-  # Load shared agent components
+  # Components
+  # Use full blocks here, rather than single line logic so that
+  # automation can insert components as needed.
   ########
 
-  instance_eval File.read(File.join(File.dirname(__FILE__), '_shared-agent-components.rb'))
+  proj.component 'runtime-agent'
+  proj.component 'libffi'
+  proj.component 'libyaml'
+  proj.component "openssl-#{proj.openssl_version}"
 
-  ########
-  # Components specific to the main branch
-  ########
+  proj.component 'curl'
+  proj.component 'puppet-ca-bundle'
+  proj.component "ruby-#{proj.ruby_version}"
 
-  # When adding components to this list, please
-  # add them to pe-installer-runtime-main as well
+  proj.component 'rubygem-base64'
   proj.component 'rubygem-concurrent-ruby'
-  proj.component 'rubygem-multi_json'
-  proj.component 'rubygem-optimist'
-  proj.component 'rubygem-highline'
+  proj.component 'rubygem-deep_merge'
+  proj.component 'rubygem-erubi'
+  proj.component 'rubygem-fast_gettext'
+  proj.component 'rubygem-ffi'
+  proj.component 'rubygem-gettext'
   proj.component 'rubygem-hiera-eyaml'
-  proj.component 'rubygem-thor'
+  proj.component 'rubygem-highline'
+  proj.component 'rubygem-hocon'
+  proj.component 'rubygem-locale'
+  proj.component 'rubygem-logger'
+  proj.component 'rubygem-multi_json'
+  proj.component 'rubygem-net-ssh'
+  proj.component 'rubygem-optimist'
+  proj.component 'rubygem-prime'
+  proj.component 'rubygem-semantic_puppet'
   proj.component 'rubygem-scanf'
+  proj.component 'rubygem-text'
+  proj.component 'rubygem-thor'
+
+  # We add rexml explicitly in here because even though ruby 3 ships with rexml as its default gem, the version
+  # of rexml it ships with can contain CVEs. So, we add it here to update to a higher version free from the CVEs.
+  proj.component 'rubygem-rexml'
+
+  unless platform.is_windows?
+    proj.component 'augeas'
+    proj.component 'ruby-augeas'
+    proj.component 'libxml2'
+    proj.component 'libxslt'
+    proj.component 'rubygem-sys-filesystem'
+  end
+
+  if platform.is_macos?
+    proj.component 'readline'
+    proj.component 'rubygem-CFPropertyList'
+  end
+
+  unless platform.is_aix? || platform.is_windows?
+    proj.component 'ruby-shadow'
+  end
+
+  # We only build ruby-selinux for EL, Fedora, Debian and Ubuntu (amd64/i386)
+  if platform.is_el? || platform.is_fedora? || platform.is_debian? || (platform.is_ubuntu? && platform.architecture !~ /ppc64el$/)
+    proj.component 'ruby-selinux'
+  end
+
+  # libedit is used instead of readline on these platforms
+  if platform.is_solaris?
+    proj.component 'libedit'
+  end
+
+  if platform.is_cross_compiled?
+    proj.component 'pl-ruby-patch'
+  end
+
+  if platform.is_windows? || platform.is_solaris? || platform.is_aix?
+    proj.component 'rubygem-minitar'
+  end
 
   if platform.is_linux?
     proj.component "virt-what"
     proj.component "dmidecode" unless platform.architecture =~ /ppc64/
   end
-
-  unless platform.is_windows?
-    proj.component 'rubygem-sys-filesystem'
-  end
-
-  # Dependencies for gettext for Ruby >= 3.2 (PA-4815)
-  proj.component 'rubygem-erubi'
-  proj.component 'rubygem-prime'
 end
