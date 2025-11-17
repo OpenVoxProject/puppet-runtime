3fc77433895f116c7eb1d1f99e9a74fc1b66a446
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-11-17 10:15:05 -0800

    Update Augeas to 0.6.0
    These are now released from GitHub. This also removes the _base file and
    cleans up the code.

--- configs/components/ruby-augeas.rb
+++ configs/components/ruby-augeas.rb
@@ -1,3 +1,90 @@
-component "ruby-augeas" do |pkg, settings, platform|
-  instance_eval File.read('configs/components/_base-ruby-augeas.rb')
+  #####
+  # Component release information:
+  #   https://github.com/hercules-team/ruby-augeas/releases
+  # Notes: 2025-11-17 - Using 0.5.0 due to issues with 0.6.0 on some platforms.
+  #####
+component 'ruby-augeas' do |pkg, settings, platform|
+  pkg.version '0.6.0'
+  pkg.sha256sum '98158a54c655b4823439b4bd38609f01e0b912a3d1453144082b8a5f43b0c4dc'
+  pkg.url "https://github.com/hercules-team/ruby-augeas/releases/download/release-#{pkg.get_version}/ruby-augeas-#{pkg.get_version}.tgz"
+  pkg.build_requires "ruby-#{settings[:ruby_version]}"
+  pkg.build_requires 'augeas'
+
+  pkg.environment 'PATH', '$(PATH):/usr/local/bin:/opt/csw/bin:/usr/ccs/bin:/usr/sfw/bin'
+  pkg.environment 'CONFIGURE_ARGS', '--vendor'
+  pkg.environment 'PKG_CONFIG_PATH', "#{File.join(settings[:libdir], 'pkgconfig')}:/usr/lib/pkgconfig"
+
+  if platform.is_aix?
+    pkg.environment 'CC', '/opt/freeware/bin/gcc'
+    pkg.environment 'PATH', '$(PATH):/opt/freeware/bin'
+    pkg.environment 'RUBY', settings[:host_ruby]
+    pkg.environment 'LDFLAGS', " -brtl #{settings[:ldflags]}"
+  end
+
+  if platform.is_solaris?
+    pkg.environment 'RUBY', settings[:host_ruby] if platform.is_cross_compiled?
+
+    if !platform.is_cross_compiled? && platform.architecture == 'sparc'
+      ruby = File.join(settings[:ruby_bindir], 'ruby')
+    else
+      # This should really only be done when cross compiling but
+      # to avoid breaking solaris x86_64 in 7.x continue preloading
+      # our hook.
+      ruby = "#{settings[:host_ruby]} -r#{settings[:datadir]}/doc/rbconfig-#{settings[:ruby_version]}-orig.rb"
+    end
+  elsif platform.is_cross_compiled? && (platform.is_linux? || platform.is_macos?)
+    pkg.environment 'RUBY', settings[:host_ruby]
+    ruby = "#{settings[:host_ruby]} -r#{settings[:datadir]}/doc/rbconfig-#{settings[:ruby_version]}-orig.rb"
+    pkg.environment 'LDFLAGS', settings[:ldflags]
+  elsif platform.is_macos?
+    pkg.environment 'PATH', '$(PATH):/opt/homebrew/bin' if platform.architecture == 'arm64'
+    pkg.environment 'CC', settings[:cc]
+    pkg.environment 'CFLAGS', settings[:cflags]
+    pkg.environment 'LDFLAGS', settings[:ldflags]
+    pkg.environment 'MACOSX_DEPLOYMENT_TARGET', settings[:deployment_target]
+    ruby = File.join(settings[:ruby_bindir], 'ruby')
+  else
+    ruby = File.join(settings[:ruby_bindir], 'ruby')
+  end
+
+  pkg.build do
+    build_commands = []
+    
+    extconf = "#{ruby} ext/augeas/extconf.rb"
+    # The pkg-config shim gets confused on Almalinux 9 that we are building on
+    # due to the version of rpm being cranky about using our older OpenSSL version,
+    # so bypass the shim and use pkgconf directly.
+    extconf += " --with-pkg-config=/usr/bin/pkgconf" if platform.name =~ /el-9/
+    build_commands << extconf
+    build_commands << "#{platform[:make]} -e -j$(shell expr $(shell #{platform[:num_cores]}) + 1)"
+
+    build_commands
+  end
+
+  if settings[:ruby_vendordir]
+    augeas_rb_target = File.join(settings[:ruby_vendordir], 'augeas.rb')
+  else
+    # If no alternate vendordir has been set, install into default
+    # vendordir for this ruby version.
+    augeas_rb_target = File.join(settings[:ruby_dir], 'lib', 'ruby', 'vendor_ruby', 'augeas.rb')
+  end
+
+  pkg.install_file 'lib/augeas.rb', augeas_rb_target
+
+  pkg.install do
+    [
+      "#{platform[:make]} -e -j$(shell expr $(shell #{platform[:num_cores]}) + 1) DESTDIR=/ install",
+    ]
+  end
+
+  if platform.is_solaris? || platform.is_cross_compiled_linux?
+    pkg.install do
+      "chown root:root #{augeas_rb_target}"
+    end
+  end
+
+  # Clean after install in case we are building for multiple rubies.
+  pkg.install do
+    "#{platform[:make]} -e clean"
+  end
 end
