f4168ab56b303d32a851ef1b3def3059481c8514
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-11-17 15:44:23 -0800

    rubygem-puppet: Add dependencies rubygem-concurrent-ruby, rubygem-deep_merge, rubygem-facter, rubygem-fast_gettext, rubygem-getoptlong, rubygem-locale, rubygem-multi_json, rubygem-puppet-resource_api, rubygem-scanf, rubygem-semantic_puppet

--- configs/components/rubygem-puppet.rb
+++ configs/components/rubygem-puppet.rb
@@ -9,6 +9,16 @@ component 'rubygem-puppet' do |pkg, settings, platform|
   ### Maintained by update_gems automation ###
   pkg.version '8.10.0'
   pkg.sha256sum 'f2f5de8f5a794ec49ac30f42806c9ffb5cd42c9908ab3014f9d8ea68a21c2a3a'
+  pkg.build_requires 'rubygem-concurrent-ruby'
+  pkg.build_requires 'rubygem-deep_merge'
+  pkg.build_requires 'rubygem-facter'
+  pkg.build_requires 'rubygem-fast_gettext'
+  pkg.build_requires 'rubygem-getoptlong'
+  pkg.build_requires 'rubygem-locale'
+  pkg.build_requires 'rubygem-multi_json'
+  pkg.build_requires 'rubygem-puppet-resource_api'
+  pkg.build_requires 'rubygem-scanf'
+  pkg.build_requires 'rubygem-semantic_puppet'
   ### End automated maintenance section ###
   
   instance_eval File.read('configs/components/_base-rubygem.rb')
