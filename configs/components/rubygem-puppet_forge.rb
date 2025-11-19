58cad28517802e126d21a78e05d6b84d4a233878
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-11-19 15:32:26 -0800

    Unpin rubygem-puppet_forge

--- configs/components/rubygem-puppet_forge.rb
+++ configs/components/rubygem-puppet_forge.rb
@@ -3,14 +3,9 @@
 #   https://rubygems.org/gems/puppet_forge
 #   https://github.com/puppetlabs/forge-ruby/releases
 #   https://github.com/puppetlabs/forge-ruby/blob/main/CHANGELOG.md
-# Notes:
-#   2025-07-23: Leaving this at 5.x for now since 6.x requires the new
-#               minitar gem, which OpenVox also uses. Need to fix it up
-#               in the agent before bumping this.
 #####
-component 'rubygem-puppet_forge' do |pkg, settings, platform|
+component 'rubygem-puppet_forge' do |pkg, _settings, _platform|
   ### Maintained by update_gems automation ###
-  # PINNED
   pkg.version '5.0.4'
   pkg.md5sum '04a2ca2f027ed41d9142ced587b71bd7'
   ### End automated maintenance section ###
