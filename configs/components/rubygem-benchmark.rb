98a60095ae8d5b472232260fec65d7c3340500fb
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-11-19 15:42:21 -0800

    rubygem-benchmark: Update component metadata

--- configs/components/rubygem-benchmark.rb
+++ configs/components/rubygem-benchmark.rb
@@ -5,7 +5,7 @@
 component 'rubygem-benchmark' do |pkg, _settings, _platform|
   ### Maintained by update_gems automation ###
   pkg.version '0.5.0'
-  pkg.sha256sum 'd4ef40037bba27f03b28013e219b950b82bace296549ec15a78016552f8d2cce'
+  pkg.sha256sum '465df122341aedcb81a2a24b4d3bd19b6c67c1530713fd533f3ff034e419236c'
   ### End automated maintenance section ###
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
