87b02cb0cb4cad0eab148370c3d046ace8fc574a
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-11-19 09:56:21 -0800

    Unpin benchmark

--- configs/components/rubygem-benchmark.rb
+++ configs/components/rubygem-benchmark.rb
@@ -4,9 +4,7 @@
 #####
 component 'rubygem-benchmark' do |pkg, settings, platform|
   ### Maintained by update_gems automation ###
-  # Remove once we do the next release of openvox
-  # PINNED
-  pkg.version '0.4.1'
+  pkg.version '0.5.0'
   pkg.sha256sum 'd4ef40037bba27f03b28013e219b950b82bace296549ec15a78016552f8d2cce'
   ### End automated maintenance section ###
 
