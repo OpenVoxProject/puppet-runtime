5e8171c5a035d90db579a6fc3ad77bdd78139bc9
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-11-19 15:34:27 -0800

    Unpin rubygem-minitar
    Will make the appropriate namespace changes in OpenVox.

--- configs/components/rubygem-minitar.rb
+++ configs/components/rubygem-minitar.rb
@@ -2,13 +2,9 @@
 # Component release information:
 #   https://rubygems.org/gems/minitar
 #   https://github.com/halostatue/minitar/blob/main/CHANGELOG.md
-# Notes:
-#   2025-07-23: 1.x changes a namespace, and there are no vulnerability fixes,
-#               so leaving this at 0.12.1 for now.
 #####
-component 'rubygem-minitar' do |pkg, settings, platform|
+component 'rubygem-minitar' do |pkg, _settings, _platform|
   ### Maintained by update_gems automation ###
-  # PINNED
   pkg.version '0.12.1'
   pkg.md5sum '975dee1dadeb26a2a01105802c3172ab'
   ### End automated maintenance section ###
