a5ecbad6251f43d6578f7776ba84c057cc1262ab
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-11-19 15:42:21 -0800

    rubygem-aws-sdk-ec2: Update 1.579.0 -> 1.580.0

--- configs/components/rubygem-aws-sdk-ec2.rb
+++ configs/components/rubygem-aws-sdk-ec2.rb
@@ -5,8 +5,8 @@
 #####
 component 'rubygem-aws-sdk-ec2' do |pkg, _settings, _platform|
   ### Maintained by update_gems automation ###
-  pkg.version '1.579.0'
-  pkg.sha256sum '45b8a1cc2cb24fbd6ec1565934954e4313a48ee281844242a58f1495ce97d6ca'
+  pkg.version '1.580.0'
+  pkg.sha256sum '0a6ab262fa57a3fec8d07d7f8f43d68ab104df0a1b6bc8f895c53b4d495f014a'
   pkg.build_requires 'rubygem-aws-sdk-core'
   pkg.build_requires 'rubygem-aws-sigv4'
   ### End automated maintenance section ###
