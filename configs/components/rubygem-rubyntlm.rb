c3d2ef17f1d141e06e2da4c906bcc794cc14866f
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-11-19 15:33:18 -0800

    Unpin rubygem-rubyntlm

--- configs/components/rubygem-rubyntlm.rb
+++ configs/components/rubygem-rubyntlm.rb
@@ -3,11 +3,8 @@
 #   https://rubygems.org/gems/rubyntlm
 #   https://github.com/WinRb/rubyntlm/releases
 #####
-component 'rubygem-rubyntlm' do |pkg, settings, platform|
-  # Do not update past this version without solving the jruby/ruby2.7 issue described in the commit
-  # message this comment is associated with.
+component 'rubygem-rubyntlm' do |pkg, _settings, _platform|
   ### Maintained by update_gems automation ###
-  # PINNED
   pkg.version '0.6.3'
   pkg.sha256sum '5b321456dba3130351f7451f8669f1afa83a0d26fd63cdec285b7b88e667102d'
   ### End automated maintenance section ###
