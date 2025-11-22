cead4bcc7a427740e25958b1c445f65a4225c7e5
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-11-21 16:23:55 -0800

    agent-runtime-main: Add rubygem-timeout component

--- configs/projects/agent-runtime-main.rb
+++ configs/projects/agent-runtime-main.rb
@@ -71,6 +71,7 @@ project 'agent-runtime-main' do |proj|
   proj.component 'rubygem-net-ftp'
   proj.component 'rubygem-time'
   proj.component 'rubygem-net-protocol'
+  proj.component 'rubygem-timeout'
   proj.component 'rubygem-net-ssh'
   proj.component 'rubygem-optimist'
   proj.component 'rubygem-prime'
