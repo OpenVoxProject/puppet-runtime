f6d7d2130b66f8c07cd01832c46708305c96c553
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-12-04 16:41:52 -0800

    Pin faraday-follow-redirects and rubyzip

--- configs/components/rubygem-faraday-follow_redirects.rb
+++ configs/components/rubygem-faraday-follow_redirects.rb
@@ -2,11 +2,14 @@
 # Component release information:
 #   https://rubygems.org/gems/faraday-follow_redirects
 #   https://github.com/tisba/faraday-follow-redirects/blob/main/CHANGELOG.md
+# Notes:
+#   2025-12-04: puppet_forge 6.0.0 has faraday-follow_redirects ~> 0.3.0
 #####
 component 'rubygem-faraday-follow_redirects' do |pkg, _settings, _platform|
   ### Maintained by update_gems automation ###
-  pkg.version '0.4.0'
-  pkg.sha256sum 'd3fa1118ab1350e24035a272b4cff64948643bb7182846db89acaf87abadd5d9'
+  # PINNED
+  pkg.version '0.3.0'
+  pkg.sha256sum 'd92d975635e2c7fe525dd494fcd4b9bb7f0a4a0ec0d5f4c15c729530fdb807f9'
   pkg.build_requires 'rubygem-faraday'
   ### End automated maintenance section ###
 
--- configs/components/rubygem-rubyzip.rb
+++ configs/components/rubygem-rubyzip.rb
@@ -3,11 +3,14 @@
 #   https://rubygems.org/gems/rubyzip
 #   https://github.com/rubyzip/rubyzip/releases
 #   https://github.com/rubyzip/rubyzip/blob/master/Changelog.md
+# Notes:
+#   2025-12-04: winrm-fs requires rubyzip ~> 2.0
 #####
 component 'rubygem-rubyzip' do |pkg, _settings, _platform|
   ### Maintained by update_gems automation ###
-  pkg.version '3.2.2'
-  pkg.sha256sum 'c0ed99385f0625415c8f05bcae33fe649ed2952894a95ff8b08f26ca57ea5b3c'
+  # PINNED
+  pkg.version '2.4.1'
+  pkg.sha256sum '8577c88edc1fde8935eb91064c5cb1aef9ad5494b940cf19c775ee833e075615'
   ### End automated maintenance section ###
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
