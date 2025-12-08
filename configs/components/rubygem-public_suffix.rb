40dbaaca0e2b8e2cbc1063cf97f61fe22b3c3cdb
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-12-08 11:03:45 -0800

    Unpin public_suffix
    The addressable gem now allows 7.x.

--- configs/components/rubygem-public_suffix.rb
+++ configs/components/rubygem-public_suffix.rb
@@ -2,11 +2,9 @@
 # Component release information:
 #   https://rubygems.org/gems/public_suffix
 #   https://github.com/weppos/publicsuffix-ruby/blob/main/CHANGELOG.md
-# Notes: 2025-11-17 - Pinned to < 7 until the addressable gem updates its dependency
 #####
 component 'rubygem-public_suffix' do |pkg, _settings, _platform|
   ### Maintained by update_gems automation ###
-  # PINNED
   pkg.version '6.0.2'
   pkg.sha256sum 'bfa7cd5108066f8c9602e0d6d4114999a5df5839a63149d3e8b0f9c1d3558394'
   ### End automated maintenance section ###
