d288bcb62d9f9d173b851aac634dadc9e0be7515
Gitii <Gitii@users.noreply.github.com>
2025-12-19 21:11:09 +0100

    align with other platform configs

--- configs/platforms/debian-13-armhf.rb
+++ configs/platforms/debian-13-armhf.rb
@@ -1,36 +1,21 @@
 platform 'debian-13-armhf' do |plat|
-  plat.servicedir '/lib/systemd/system'
-  plat.defaultdir '/etc/default'
-  plat.servicetype 'systemd'
-  plat.codename 'trixie'
-
-  plat.install_build_dependencies_with 'DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends '
-
+  plat.inherit_from_default
   packages = [
     'build-essential',
-    'make',
-    'quilt',
-    'pkg-config',
+    'cmake',
     'debhelper',
-    'rsync',
+    'devscripts',
     'fakeroot',
     'libbz2-dev',
     'libreadline-dev',
     'libselinux1-dev',
     'make',
     'pkg-config',
-    'cmake',
-    'gcc',
+    'quilt',
+    'rsync',
     'swig',
     'systemtap-sdt-dev',
     'zlib1g-dev'
   ]
-
-  plat.provision_with "export DEBIAN_FRONTEND=noninteractive && apt-get update -qq && apt-get install -qy --no-install-recommends #{packages.join(' ')}"
-
-  # Not currently used
-  plat.docker_image 'debian:13'
-  plat.docker_registry 'docker.io'
-  plat.docker_arch 'linux/arm/v7'
-
+  plat.provision_with "export DEBIAN_FRONTEND=noninteractive; apt-get update -qq; apt-get install -qy --no-install-recommends #{packages.join(' ')}"
 end
