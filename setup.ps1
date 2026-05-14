cc85db1dd65e08f5103cec43831f0f47b40a4223
Sebastian Rakel <sebastian@devunit.eu>
2026-05-14 22:06:54 +0200

    workaround, work with cygwin dir

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
 $url="https://repo.msys2.org/distrib/msys2-x86_64-latest.exe"
 $dest="C:\msys2-x86_64-latest.exe"
 Invoke-WebRequest -Uri $url -OutFile $dest
-cmd /c "C:\msys2-x86_64-latest.exe in --confirm-command --accept-messages --root C:/msys64"
+cmd /c "C:\msys2-x86_64-latest.exe in --confirm-command --accept-messages --root C:/cygwin64"
