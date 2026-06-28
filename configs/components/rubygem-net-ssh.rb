2ad75f9d36b6a3299e5d3358c1dbe883046a4e23
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2026-06-28 00:37:53 +0000

    Update rubygem components
    Component updates:
    - rubygem-net-ssh: version 7.3.2 -> 7.3.3
    
    Signed-off-by: OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>

--- configs/components/rubygem-net-ssh.rb
+++ configs/components/rubygem-net-ssh.rb
@@ -5,8 +5,8 @@
 #####
 component 'rubygem-net-ssh' do |pkg, _settings, _platform|
   ### Maintained by update_gems automation ###
-  pkg.version '7.3.2'
-  pkg.sha256sum '65029e213c380e20e5fd92ece663934ab0a0fe888e0cd7cc6a5b664074362dd4'
+  pkg.version '7.3.3'
+  pkg.sha256sum '831def58b2c51dcef66ec00d29397d4f210de89c19fe78f95873ca30f386e86a'
   ### End automated maintenance section ###
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
