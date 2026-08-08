788777a4cafe03220ae09d2af3aa684952e98c5b
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2026-08-08 00:13:42 +0000

    Update rubygem components
    Component updates:
    - rubygem-aws-partitions: version 1.1277.0 -> 1.1279.0
    - rubygem-aws-sdk-core: version 3.254.0 -> 3.254.1
    - rubygem-aws-sdk-ec2: version 1.633.0 -> 1.636.0
    - rubygem-openfact: version 5.7.0 -> 6.0.0
    
    Signed-off-by: OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>

--- configs/components/rubygem-aws-partitions.rb
+++ configs/components/rubygem-aws-partitions.rb
@@ -5,8 +5,8 @@
 #####
 component 'rubygem-aws-partitions' do |pkg, _settings, _platform|
   ### Maintained by update_gems automation ###
-  pkg.version '1.1277.0'
-  pkg.sha256sum '40bda996876a45a60c43fbf489b04b46216e98c1814c1ac6453b942e0df6501e'
+  pkg.version '1.1279.0'
+  pkg.sha256sum '9baa4f75bca3e5efe4d4df27655ae640aaaa1f6e2d3cf59fda3dad19689ae435'
   ### End automated maintenance section ###
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
