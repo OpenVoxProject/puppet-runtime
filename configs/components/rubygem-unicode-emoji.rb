3579435b1267ff2d580851b788fe3f70fab9f51d
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2026-09-21 00:19:13 +0000

    Update rubygem components
    Component updates:
    - rubygem-unicode-display_width: version 3.2.0 -> 3.3.0
    - rubygem-unicode-emoji: version 4.2.0 -> 4.3.0
    
    Signed-off-by: OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>

--- configs/components/rubygem-unicode-emoji.rb
+++ configs/components/rubygem-unicode-emoji.rb
@@ -4,8 +4,8 @@
 #####
 component 'rubygem-unicode-emoji' do |pkg, _settings, _platform|
   ### Maintained by update_gems automation ###
-  pkg.version '4.2.0'
-  pkg.sha256sum '519e69150f75652e40bf736106cfbc8f0f73aa3fb6a65afe62fefa7f80b0f80f'
+  pkg.version '4.3.0'
+  pkg.sha256sum '11c02fa73290378c066bb0562cd4c87d8e1b706fbbe0059ca12746a4244de8ce'
   ### End automated maintenance section ###
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
