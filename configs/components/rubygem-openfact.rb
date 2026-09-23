7d9f24cc7e5d14c23c62785ee865c17e6fe93096
Tim Meusel <tim@bastelfreak.de>
2026-09-23 17:38:00 +0200

    openfact: Update 5.7.0->5.7.1
    Signed-off-by: Tim Meusel <tim@bastelfreak.de>

--- configs/components/rubygem-openfact.rb
+++ configs/components/rubygem-openfact.rb
@@ -6,8 +6,8 @@
 component 'rubygem-openfact' do |pkg, _settings, _platform|
   ### Maintained by update_gems automation ###
   # PINNED
-  pkg.version '5.7.0'
-  pkg.sha256sum 'f540d77d93259d410a19e6aebc61f81e78e63c275f8eb01ffb832fdfc87f298c'
+  pkg.version '5.7.1'
+  pkg.sha256sum 'b67f3f2a00eb383be57fa0f0d8ee9b847b0f661df20eebde97a805c9a6e3e01e'
   pkg.build_requires 'rubygem-base64' if settings[:ruby_version] == '3.2'
   pkg.build_requires 'rubygem-hocon'
   pkg.build_requires 'rubygem-thor'
